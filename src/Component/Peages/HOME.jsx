1cf460f8538b99e60cff9fed009f48a528b85973
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2025-08-06 00:01:10 +0300

    about done

--- src/Component/Peages/HOME.jsx
+++ src/Component/Peages/HOME.jsx
@@ -3,6 +3,7 @@ import Header from '../Header/Header'
 import Header2 from '../Header/Header2'
 import Hero from '../Hero/Hero'
 import Popular from '../Popular/Popular'
+import About from '../About/About'
 
 const HOME = () => {
   return (
@@ -11,6 +12,7 @@ const HOME = () => {
     <Header2 />
     <Hero />
     <Popular />
+    <About />
     </>
 )
 }

daec8f94779966216d965b57ebbd1541c6cccfe5
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2025-08-10 18:38:58 +0300

    Menu done

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,44 +1,52 @@
 import React from 'react'
 import MenuJson from "./MenuJson.json"
 import "./Menu.css"
+import {motion, AnimatePresence } from 'framer-motion'
 
 const Menu = () => {
+  const [fillter, setFillter] = React.useState("all")
+
+  const category = ["all", "breakfast", "pizza", "pasta", "fast food"]
+  const filltered = fillter === "all" ? MenuJson : MenuJson.filter(item => item.MenuType.toLowerCase() === fillter.toLowerCase());
+  const repeatedItems = [...MenuJson, ...MenuJson];
   return (
-    <section className='p-20 bg-slate-100'>
+    <>
+    <section className='lg:p-20 md:p-10 sm:p-5 p-5 bg-slate-100'>
       <div className="menu text-center lg:p-10 md:p-10 sm:p-5 p-2 bg-white rounded-3xl">
         <h4>FOOD MENU</h4>
         <h2>Fresheat Foods Menu</h2>
-        <ul className="fillter flex justify-center items-center lg:gap-20 md:gap-10 sm:gap-5 gap-2 mb-5">
-          <li>all</li>
-          <li>breakfast</li>
-          <li>pizza</li>
-          <li>pasta</li>
+        <ul className="fillter flex justify-center items-center flex-wrap lg:gap-20 md:gap-10 sm:gap-5 gap-5 mb-5">
+          {category.map((item) => (
+            <li key={item} onClick={() => setFillter(item)} className={fillter === item ? "active" : ""}>{item}</li>
+          ))}
         </ul>
         <hr className='my-10 border-slate-800 '/>
         <div className="menu-items">
-          {MenuJson.map((item) => (
-            <div key={item.id} className="menu-card lg:w-1/2 md:w-1/2 sm:w-full w-full">
-              <div className="flex justify-center items-center">
-                <img src={require(`../../IMG/${item.image}`)} alt={item.name} />
-                <div className="text-start"> 
-                  <h3>{item.name}</h3>
-                  <h4>{item.description}</h4>
+          <AnimatePresence>
+            {filltered.map((item) => (
+              <motion.div key={item.id} className="menu-card lg:w-1/2 md:w-1/2 sm:w-full w-full" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -50 }} transition={{ duration: 0.4 }}>
+                <div className="flex justify-center items-center">
+                  <img src={require(`../../IMG/${item.image}`)} alt={item.name} />
+                  <div className="text-start"> 
+                    <h3>{item.name}</h3>
+                    <h4>{item.description}</h4>
+                  </div>
                 </div>
-              </div>
-              <p>${item.price}</p>
-            </div>
-          ))}
+                <p>${item.price}</p>
+              </motion.div>
+            ))}
+          </AnimatePresence>     
         </div>
       </div>
-
-      <div>
-        <ul>
-          {MenuJson.map((item) => (
-            <li key={item.id}>{item.name}</li>
+    </section>
+    <div className="loop-container bg-slate-100">
+        <ul className='loop-list'>
+          {repeatedItems.map((item, index) => (
+            <li key={index}>{item.name}</li>
           ))}
         </ul>
       </div>
-    </section>
+    </>
   )
 }
 
